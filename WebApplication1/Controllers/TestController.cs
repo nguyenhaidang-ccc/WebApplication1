3c50e12664585d1378e3cb1860400b4bb0c5b571
admin <admin@EDWFTTGW>
2022-08-31 15:43:49 +0700

    Add project files.

---
+++ WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Message from ViewBag";
+
+            ViewBag.CurrentTime = DateTime.Now;
+            return View();
+        }
+    }
+}
